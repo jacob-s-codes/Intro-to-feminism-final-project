429c0693e314d319185c7a44047332f3f81b2c97
jacob-s-codes <jacobbshaul@gmail.com>
2024-11-19 13:56:36 -0800

    see if this works on push

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*.html", "*/index.html", "*/**/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        customFont: ['Sour Gummy', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
